6ff6bcb60dd81a952184afdc7468aae2f52fc3b1
Matthew Jia <mcj57@cornell.edu>
2024-10-29 22:00:57 -0400

    Implement starting page GUI

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,34 @@
-let () = print_endline "Theme: Fall Fun"
+open GMain
+open GPango
+
+(* Initialize GTK *)
+let () = ignore (GMain.init ())
+
+(* Create a new window *)
+let window = GWindow.window ~title:"OCaml Strands" ~border_width:10 ()
+
+(* Execute *)
+let () =
+  (* Set up exit function when the window is closed *)
+  ignore (window#connect#destroy ~callback:Main.quit);
+  (* Create vertical element box with 20 px of padding *)
+  let vbox = GPack.vbox ~border_width:20 ~packing:window#add () in
+  (* Create game title and subtitle with font 20 *)
+  let title_label = GMisc.label ~text:"OCaml Strands" ~packing:vbox#pack () in
+  title_label#misc#modify_font (GPango.font_description_from_string "Serif 20");
+  ignore
+    (GMisc.label ~text:"By: Falak, Amy, Angie, and Matthew" ~packing:vbox#pack
+       ());
+
+  (* Create a start button to execute program *)
+  let start_button = GButton.button ~label:"Play" ~packing:vbox#pack () in
+  (* Set up a callback for the button click event *)
+  ignore
+    (start_button#connect#clicked ~callback:(fun () ->
+         print_endline "Substitute function here"));
+
+  (* Show all widgets *)
+  window#show ();
+
+  (* Start the GTK main loop *)
+  Main.main ()
