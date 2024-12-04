0418fda0d0c710c94b3af2fc8c204f245a28ae05
Angelina Chen <angelina.chhen@gmail.com>
2024-12-03 22:18:34 -0500

    Put all grid / board data into separate file for code cleanliness

--- bin/main.ml
+++ bin/main.ml
@@ -2,6 +2,7 @@ open GMain
 open GPango
 open Printf
 open Cs3110_fin.Logic
+open Cs3110_fin.Grid_data
 
 (* Initialize GTK *)
 let () = ignore (GMain.init ())
@@ -13,113 +14,6 @@ let max_hints = 7
 
 (* Create a new window *)
 let window = GWindow.window ~title:"OCaml Strands" ~border_width:10 ()
-
-(* Spangram: Orchards (?) *)
-let initial_grid : grid =
-  [|
-    [| 'S'; 'N'; 'P'; 'M'; 'D'; 'S' |];
-    [| 'I'; 'K'; 'U'; 'I'; 'R'; 'E' |];
-    [| 'E'; 'A'; 'M'; 'P'; 'H'; 'Y' |];
-    [| 'Z'; 'R'; 'H'; 'A'; 'D'; 'A' |];
-    [| 'O'; 'U'; 'C'; 'G'; 'R'; 'S' |];
-    [| 'T'; 'C'; 'N'; 'H'; 'U'; 'P' |];
-    [| 'R'; 'S'; 'I'; 'O'; 'P'; 'L' |];
-    [| 'E'; 'D'; 'D'; 'A'; 'E'; 'S' |];
-  |]
-
-(* Spangram: Medical career *)
-let to_your_health : grid =
-  [|
-    [| 'S'; 'N'; 'U'; 'M'; 'A'; 'R' |];
-    [| 'E'; 'R'; 'E'; 'D'; 'H'; 'M' |];
-    [| 'D'; 'E'; 'I'; 'P'; 'A'; 'C' |];
-    [| 'N'; 'C'; 'A'; 'L'; 'T'; 'I' |];
-    [| 'T'; 'I'; 'S'; 'T'; 'C'; 'S' |];
-    [| 'E'; 'O'; 'E'; 'R'; 'A'; 'R' |];
-    [| 'G'; 'N'; 'E'; 'C'; 'T'; 'O' |];
-    [| 'R'; 'U'; 'S'; 'R'; 'O'; 'D' |];
-  |]
-
-(* Spangram: Well-suited *)
-let nice_fit : grid =
-  [|
-    [| 'P'; 'M'; 'L'; 'E'; 'R'; 'E' |];
-    [| 'U'; 'T'; 'H'; 'Y'; 'I'; 'U' |];
-    [| 'J'; 'I'; 'R'; 'D'; 'A'; 'S' |];
-    [| 'B'; 'L'; 'L'; 'S'; 'U'; 'D' |];
-    [| 'W'; 'E'; 'B'; 'I'; 'T'; 'E' |];
-    [| 'S'; 'U'; 'S'; 'G'; 'N'; 'O' |];
-    [| 'I'; 'E'; 'C'; 'S'; 'L'; 'R' |];
-    [| 'N'; 'I'; 'V'; 'I'; 'S'; 'T' |];
-  |]
-
-(* Spangram: Social Media *)
-let extremely_online : grid =
-  [|
-    [| 'T'; 'W'; 'S'; 'S'; 'H'; 'O' |];
-    [| 'E'; 'O'; 'R'; 'A'; 'F'; 'L' |];
-    [| 'E'; 'C'; 'E'; 'W'; 'O'; 'L' |];
-    [| 'T'; 'I'; 'C'; 'M'; 'T'; 'N' |];
-    [| 'A'; 'E'; 'O'; 'A'; 'M'; 'E' |];
-    [| 'R'; 'L'; 'K'; 'L'; 'I'; 'P' |];
-    [| 'E'; 'P'; 'M'; 'I'; 'D'; 'O' |];
-    [| 'T'; 'R'; 'O'; 'E'; 'S'; 'T' |];
-  |]
-
-let target_words =
-  [ "hayrides"; "pumpkins"; "maze"; "cider"; "doughnuts"; "apples"; "orchards" ]
-
-let extremely_online_target =
-  [
-    "tweet";
-    "report";
-    "share";
-    "follow";
-    "comment";
-    "post";
-    "like";
-    "report";
-    "socialmedia";
-  ]
-
-let nice_fit_target =
-  [ "jump"; "birthday"; "leisure"; "business"; "civil"; "strong"; "wellsuited" ]
-
-let to_your_health_target =
-  [ "nurse"; "dentist"; "medicalcareer"; "surgeon"; "pharmacist"; "doctor" ]
-
-let word_positions =
-  [
-    ( "hayrides",
-      [ (2, 4); (3, 5); (2, 5); (1, 4); (1, 3); (0, 4); (1, 5); (0, 5) ] );
-    ( "pumpkins",
-      [ (2, 3); (1, 2); (0, 3); (0, 2); (1, 1); (1, 0); (0, 1); (0, 0) ] );
-    ("maze", [ (2, 2); (2, 1); (3, 0); (2, 0) ]);
-    ("cider", [ (5, 1); (6, 2); (7, 1); (7, 0); (6, 0) ]);
-    ( "doughnuts",
-      [ (7, 2); (6, 3); (5, 4); (4, 3); (5, 3); (5, 2); (4, 1); (5, 0); (6, 1) ]
-    );
-    ("apples", [ (7, 3); (6, 4); (5, 5); (6, 5); (7, 4); (7, 5) ]);
-    ( "orchards",
-      [ (4, 0); (3, 1); (4, 2); (3, 2); (3, 3); (4, 4); (3, 4); (4, 5) ] );
-  ]
-
-(** TODO: FIX*)
-let to_your_health_position =
-  [
-    ( "hayrides",
-      [ (3, 4); (1, 5); (2, 5); (1, 4); (1, 3); (0, 4); (1, 5); (0, 5) ] );
-    ( "pumpkins",
-      [ (2, 3); (1, 2); (0, 3); (0, 2); (1, 1); (1, 0); (0, 1); (0, 0) ] );
-    ("maze", [ (2, 2); (2, 1); (3, 0); (2, 0) ]);
-    ("cider", [ (5, 1); (6, 2); (7, 1); (7, 0); (6, 0) ]);
-    ( "doughnuts",
-      [ (7, 2); (6, 3); (5, 4); (4, 3); (5, 3); (5, 2); (4, 1); (5, 0); (6, 1) ]
-    );
-    ("apples", [ (7, 3); (6, 4); (5, 5); (6, 5); (7, 4); (7, 5) ]);
-    ( "orchards",
-      [ (4, 0); (3, 1); (4, 2); (3, 2); (3, 3); (4, 4); (3, 4); (4, 5) ] );
-  ]
 
 type game_state = Cs3110_fin.Logic.game_state
 let initialize_game grid words = { grid; found_words = [] }
@@ -166,7 +60,7 @@ let rec game_loop state match_counter hint_counter max_hints accepted_words =
     exit 0)
   else game_loop new_state match_counter hint_counter max_hints accepted_words
 
-(* Handle theme selection and starts the game in terminal *)
+(* Handle all the theme selection *)
 let select_theme () =
   print_endline "Choose a theme to play:";
   print_endline "1. Fall Fun";
@@ -175,11 +69,11 @@ let select_theme () =
   print_endline "4. Extremely Online";
   let theme_choice = read_int () in
   match theme_choice with
-  | 1 -> initial_grid, target_words, word_positions
-  | 2 -> nice_fit, nice_fit_target, word_positions
-  | 3 -> to_your_health, to_your_health_target, to_your_health_position
-  | 4 -> extremely_online, extremely_online_target, word_positions
-  | _ -> initial_grid, target_words, word_positions
+  | 1 -> GridData.initial_grid, GridData.target_words, GridData.word_positions
+  | 2 -> GridData.nice_fit, GridData.nice_fit_target, GridData.word_positions
+  | 3 -> GridData.to_your_health, GridData.to_your_health_target, GridData.word_positions
+  | 4 -> GridData.extremely_online, GridData.extremely_online_target, GridData.word_positions
+  | _ -> GridData.initial_grid, GridData.target_words, GridData.word_positions
 
 let print_theme_info grid theme word_positions =
   print_endline ("Theme: " ^ theme);
