bfed01e01787e3e2fd9bb7346f8e835ae8aaa7ac
Falak <fr273@cornell.edu>
2024-12-07 16:29:16 -0500

    unlock and highlight hint

--- lib/logic.ml
+++ lib/logic.ml
@@ -59,29 +59,69 @@ let handle_guess state guess target_words =
   then { state with found_words = BatSet.add lowercase_guess state.found_words }
   else state
 
-(** Check word input, update counters, and display appropriate messages *)
+let word_to_highlight state theme_target_words =
+  List.find_opt
+    (fun word -> not (BatSet.mem word state.found_words))
+    theme_target_words
+
+let hint_highlighter hint_word word_positions grid =
+  match List.assoc_opt hint_word word_positions with
+  | Some positions ->
+      Array.iteri
+        (fun r row ->
+          Array.iteri
+            (fun c letter ->
+              let highlight = List.mem (r, c) positions in
+              print_letter letter highlight)
+            row;
+          print_newline ())
+        grid
+  | None -> Printf.printf "Hint word not found in word positions.\n"
+
+let hint_revealer state word_positions target_words accepted_words =
+  match word_to_highlight state target_words with
+  | None -> Printf.printf "Hints not available. All words have been found!\n"
+  | Some hint_word ->
+      let valid_guesses_count =
+        BatSet.filter
+          (fun word -> BatSet.mem word accepted_words)
+          state.guessed_words
+        |> BatSet.to_list |> List.length
+      in
+      if valid_guesses_count < 3 then
+        Printf.printf
+          "Sorry, hint not unlocked yet. Words left to unlock hint: %d\n"
+          (3 - valid_guesses_count)
+      else (
+        Printf.printf "Hint word: %s\n" hint_word;
+        hint_highlighter hint_word word_positions state.grid)
 
 let process_input state word target_words match_counter hint_counter max_hints
-    accepted_words =
-  let guessed_words_updated = BatSet.add word state.guessed_words in
-  if BatSet.diff guessed_words_updated state.guessed_words = BatSet.empty then (
-    Printf.printf "You've already guessed that word!\n";
-    state)
-  else if List.mem word target_words then (
-    incr match_counter;
-    Printf.printf "Match found! Total matches: %d\n" !match_counter;
-    {
-      state with
-      found_words = BatSet.add word state.found_words;
-      guessed_words = guessed_words_updated;
-    })
-  else if BatSet.mem word accepted_words then (
-    (* Check with BatSet *)
-    incr hint_counter;
-    Printf.printf "Hint incremented. Total hints: %d\n" !hint_counter;
-    Printf.printf "Current hint count: %d/%d\n" !hint_counter max_hints;
-    if !hint_counter >= max_hints then Printf.printf "Hint available!\n";
-    { state with guessed_words = guessed_words_updated })
-  else (
-    Printf.printf "Invalid word: %s\n" word;
-    { state with guessed_words = guessed_words_updated })
+    accepted_words word_positions =
+  match String.lowercase_ascii word with
+  | "hint" ->
+      Printf.printf "Processing hint request...\n";
+      hint_revealer state word_positions target_words accepted_words;
+      state (* Return the same state after processing a hint *)
+  | _ ->
+      let guessed_words_updated = BatSet.add word state.guessed_words in
+      if BatSet.mem word state.guessed_words then (
+        Printf.printf "You've already guessed that word!\n";
+        state)
+      else if List.mem word target_words then (
+        incr match_counter;
+        Printf.printf "Match found! Total matches: %d\n" !match_counter;
+        {
+          state with
+          found_words = BatSet.add word state.found_words;
+          guessed_words = guessed_words_updated;
+        })
+      else if BatSet.mem word accepted_words then (
+        incr hint_counter;
+        Printf.printf
+          "Word count towards hint incremented. Total words guessed: %d\n"
+          !hint_counter;
+        { state with guessed_words = guessed_words_updated })
+      else (
+        Printf.printf "Invalid word: %s\n" word;
+        { state with guessed_words = guessed_words_updated })
